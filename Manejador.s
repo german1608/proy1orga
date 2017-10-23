2b401e757fd36dfc40eefd3d386f5d465fc29a25
yuniquintero <yuni.quintero@gmail.com>
2017-10-23 13:37:38 -0400

    Manejador.s: Anade init()

--- Manejador.s
+++ Manejador.s
@@ -2,3 +2,17 @@
 # Autores:
 #   Yuni Quintero
 #   German Robayo
+	.text
+	.globl main
+main:
+	li $a0, 4
+# Descripcion:inicializa el manejador de memoria.
+# Argumentos:
+#	$a0: Numero de bytes a ser administrados.
+# Retorno:
+#	$v0
+	.text
+	.globl init
+init:
+	li $v0, 9
+	syscall
